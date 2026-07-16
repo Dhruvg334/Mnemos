a0e7a8fcc75ee68937364fdb620bcd793e33580a
pavitagrawal <pavitagrawal@gmail.com>
2026-07-16 16:19:10 +0530

    v4

--- src/mnemos/agentic/utils/__init__.py
+++ src/mnemos/agentic/utils/__init__.py
@@ -1,4 +1,4 @@
 from mnemos.agentic.utils.exceptions import AgenticError, ReasoningError, RetrievalError
-from mnemos.agentic.utils.logging import StructuredLogger, setup_trace, get_trace_id
+from mnemos.agentic.utils.logging import StructuredLogger, get_trace_id, setup_trace
 
 __all__ = ["StructuredLogger", "setup_trace", "get_trace_id", "AgenticError", "RetrievalError", "ReasoningError"]
